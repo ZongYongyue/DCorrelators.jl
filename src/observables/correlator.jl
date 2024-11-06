38c3ea781e07a5d3c119c659de6592a7e5125390
ZongYongyue <zongyyphy@qq.com>
2024-11-06 18:22:56 +0800

    fix dcorrelator

--- src/observables/correlator.jl
+++ src/observables/correlator.jl
@@ -36,13 +36,9 @@ struct RetardedGF{K} end
 RetardedGF(::Type{RetardedGF{:f}}) = 1
 RetardedGF(::Type{RetardedGF{:b}}) = -1
 
-struct GreaterGF end
-struct LesserGF end
-struct GorkovGF end
-struct MatsubaraGF end
 
-function dcorrelator(::Type{R}, H::MPOHamiltonian, groundenergy::Number, mps::Vector{<:FiniteMPS}; dt::Number=0.05, ft::Number=5.0, n::Integer=3, trscheme=truncdim(50)) where R<:RetardedGF
-    factor₁, factor₂, half = exp(im*groundenergy*dt), RetardedGF(R)*exp(-im*groundenergy*dt), length(mps)÷2
+function dcorrelator(::Type{R}, H::MPOHamiltonian, gsenergy::Number, mps::Vector{<:FiniteMPS}; dt::Number=0.05, ft::Number=5.0, n::Integer=3, trscheme=truncdim(50)) where R<:RetardedGF
+    t, half = collect(0:dt:ft), length(mps)÷2
     gf = SharedArray{ComplexF64, 3}(length(mps), half, length(0:dt:ft))
     @sync @distributed for i in 1:length(mps)
         if i <= half
@@ -51,6 +47,55 @@ function dcorrelator(::Type{R}, H::MPOHamiltonian, groundenergy::Number, mps::Ve
             gf[i,:,:] = propagator(H, mps[(half+1):end], mps[i]; rev=true, dt=dt, ft=ft, n=n, trscheme=trscheme)
         end
     end
-    return factor₁*gf[1:half,:,:] + factor₂*gf[(half+1):end,:,:]
+    for i in eachindex(t)
+        factor₁, factor₂ = -im*exp(im*gsenergy*t[i]), -im*exp(-im*gsenergy*t[i])
+        gf[1:half,:,i] = factor₁*gf[1:half,:,i]
+        gf[(half+1):end,:,i] = factor₂*gf[(half+1):end,:,i]
+    end
+    return gf[1:half,:,:] + RetardedGF(R)*gf[(half+1):end,:,:]
 end
 
+struct GreaterLessGF end
+
+function dcorrelator(::Type{GreaterLessGF}, H::MPOHamiltonian, mps::Vector{<:FiniteMPS}; whichs=:both, dt::Number=0.05, ft::Number=5.0, n::Integer=3, trscheme=truncdim(50))
+    t, half = collect(0:dt:ft), length(mps)÷2
+    if whichs == :both
+        gf = SharedArray{ComplexF64, 3}(length(mps), half, length(0:dt:ft))
+        @sync @distributed for i in 1:length(mps)
+            if i <= half
+                gf[i,:,:] = propagator(H, mps[1:half], mps[i]; rev=false, dt=dt, ft=ft, n=n, trscheme=trscheme) 
+            else
+                gf[i,:,:] =  propagator(H, mps[(half+1):end], mps[i]; rev=true, dt=dt, ft=ft, n=n, trscheme=trscheme)
+            end
+        end
+        for i in eachindex(t)
+            factor₁, factor₂ = -im*exp(im*gsenergy*t[i]), -im*exp(-im*gsenergy*t[i])
+            gf[1:half,:,i] = factor₁*gf[1:half,:,i]
+            gf[(half+1):end,:,i] = factor₂*gf[(half+1):end,:,i]
+        end
+        return [gf[1:half,:,:], gf[(half+1):end,:,:]]
+    elseif whichs == :greater
+        gf = SharedArray{ComplexF64, 3}(half, half, length(0:dt:ft))
+        @sync @distributed for i in 1:half
+            gf[i,:,:] = propagator(H, mps[1:end], mps[i]; rev=false, dt=dt, ft=ft, n=n, trscheme=trscheme) 
+        end
+        for i in eachindex(t)
+            gf[:,:,i] = -im*exp(im*gsenergy*t[i])*gf[:,:,i]
+        end
+        return gf
+    elseif whichs == :less
+        gf = SharedArray{ComplexF64, 3}(half, half, length(0:dt:ft))
+        @sync @distributed for i in 1:half
+            gf[i,:,:] =  propagator(H, mps[1:end], mps[i]; rev=true, dt=dt, ft=ft, n=n, trscheme=trscheme)
+        end
+        for i in eachindex(t)
+            gf[:,:,i] = -im*exp(-im*gsenergy*t[i])*gf[:,:,i]
+        end
+        return gf
+    end
+end
+
+
+
+struct GorkovGF end
+struct MatsubaraGF end
