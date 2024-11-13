be3d6dff61dddb65dc9ae0922577fa93d69d9ce4
ZongYongyue <zongyyphy@qq.com>
2024-11-13 19:05:47 +0800

    add new function

--- src/DCorrelators.jl
+++ src/DCorrelators.jl
@@ -2,26 +2,31 @@ module DCorrelators
 
 using TensorKit
 using MPSKit
-using MPSKitModels: Sector, contract_onesite, contract_twosite, @mpoham, vertices, InfiniteChain, _firstspace, _lastspace
+using MPSKitModels: Sector, contract_onesite, contract_twosite, @mpoham, vertices, nearest_neighbours, InfiniteChain, _firstspace, _lastspace
 using Distributed
 using SharedArrays
 
 import MPSKit: propagator
 
-export e_plus, e_min, fZ, S_plus, S_min, S_z, S_square, b_plus, b_min
+export hubbard
+
+export fZ, e_plus, e_min, number, onsiteCoulomb, S_plus, S_min, S_z, S_square, b_plus, b_min
 export chargedMPO
 
 export add_single_util_leg, setprocs
 
-export chargedMPS
+export chargedMPS, randFiniteMPS
 
 export propagator, dcorrelator
 export RetardedGF, GreaterLessGF
 
+include("models/hubbard.jl")
 include("operators/fermions.jl")
-include("operators/MPO/chargedMPO.jl")
+include("operators/bosons.jl")
+include("operators/chargedmpo.jl")
 include("tools.jl")
 include("states/chargedmps.jl")
+include("states/randmps.jl")
 include("observables/correlator.jl")
 
 end #module
