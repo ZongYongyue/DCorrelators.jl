8d482d314eacc87b8a01e5dc6cda21d5938d7c18
ZongYongyue <zongyyphy@qq.com>
2024-11-18 17:21:16 +0800

    fix chargedMPO

--- src/operators/chargedmpo.jl
+++ src/operators/chargedmpo.jl
@@ -2,13 +2,13 @@
     chargedMPO(operator::AbstractTensorMap, site::Integer, nsites::Integer)
 """
 function chargedMPO(operator::AbstractTensorMap, site::Integer, nsites::Integer)
-    Z, pspace = fZ(operator), domain(operator)[1]
+    pspace = domain(operator)[1]
     if (length(domain(operator)) == 2)&&(length(codomain(operator)) == 1)
-        vspace = domain(operator)[2]
+        Z, vspace = fZ(operator), domain(operator)[2]
         I = isomorphism(storagetype(operator), oneunit(vspace)*pspace, pspace*oneunit(vspace))
         mpo = FiniteMPO([i < site ? I : i == site ? add_single_util_leg(operator) : Z for i in 1:nsites])
     elseif (length(codomain(operator)) == 2)&&(length(domain(operator)) == 1)
-        vspace = codomain(operator)[1]
+        Z, vspace = fZ(operator), codomain(operator)[1]
         I = isomorphism(storagetype(operator), oneunit(vspace)*pspace, pspace*oneunit(vspace))
         mpo = FiniteMPO([i < site ? Z : i == site ? add_single_util_leg(operator) : I for i in 1:nsites])
     elseif (length(codomain(operator)) == 1)&&(length(domain(operator)) == 1)
